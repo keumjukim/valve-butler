587974fc4a24d8b89c79456852f06f2a1b2e8513
keumjukim <keumjukim@bespinglobal.com>
2020-10-15 09:55:01 +0900

    change namespace check command

--- src/com/opsnow/valve/Butler.groovy
+++ src/com/opsnow/valve/Butler.groovy
@@ -142,7 +142,8 @@ def env_namespace(namespace = "") {
     }
 
     // check namespace
-    count = sh(script: "kubectl get ns $namespace 2>&1 | grep Active | grep $namespace | wc -l", returnStdout: true).trim()
+    count = sh(script: "kubectl get ns $namespace | grep Active | grep $namespace | wc -l", returnStdout: true).trim()
+    echo "active namespace($namespace) count: $count"
     if ("$count" == "0") {
         sh "kubectl create namespace $namespace"
     }
